94e1da0374d1a37aa57fdb1512b14092ef9e3826
sbhoyar1 <40805757+sbhoyar1@users.noreply.github.com>
2018-10-18 17:40:22 -0400

    Add files via upload

---
+++ MATLAB Files Final/InVitroLiveCell_FunctionsReferred/MCF7_colony_preprocess_GFP.m
@@ -0,0 +1,17 @@
+function [I_out] = MCF7_colony_preprocess_GFP(I)
+
+
+%figure, imshow(I), title('Original');
+
+I_out = imtophat(I, strel('disk', 100));
+%figure, imshow(I_out), title('TopHat filter');
+
+I_out = imgaussfilt(I_out, 16);
+% I_out = imadjust(I_out, stretchlim(I_out, 0.0001)); 
+
+I_out = imadjust(I_out, [0, 0.4]);
+%figure, imshow(I_out), title('GFP - Adjust');
+
+
+I_out = imbinarize(I_out, 0.25);
+%figure, imshow(I_out), title('GFP - Binarized');
